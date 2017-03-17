884b9f85e7db11476764b43bfe34110987c18f10
cbq <1337715103@qq.com>
2017-03-17 22:14:08 +0800

    完成遍历省市县三级列表的功能。

---
+++ src/util/Utility.java
@@ -0,0 +1,74 @@
+package util;
+
+import model.City;
+import model.County;
+import model.Province;
+import android.text.TextUtils;
+import db.CoolWeatherDB;
+
+public class Utility {
+	/**
+	 * 解析和处理服务器返回的省级数据
+	 */
+	public synchronized static boolean handleProvincesResponse(
+			CoolWeatherDB coolWeatherDB, String response) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allProvinces = response.split(",");
+			if (allProvinces != null && allProvinces.length > 0) {
+				for (String p : allProvinces) {
+					String[] array = p.split("\\|");
+					Province province = new Province();
+					province.setProvinceCode(array[0]);
+					province.setProvinceName(array[1]);
+					coolWeatherDB.saveProvince(province);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 解析和处理服务器返回的市级数据
+	 */
+	public synchronized static boolean handleCitiesResponse(
+			CoolWeatherDB coolWeatherDB, String response, int provinceId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCiyies = response.split(",");
+			if (allCiyies != null && allCiyies.length > 0) {
+				for (String p : allCiyies) {
+					String[] array = p.split("\\|");
+					City city = new City();
+					city.setCityCode(array[0]);
+					city.setCityName(array[1]);
+					city.setProvinceId(provinceId);
+					coolWeatherDB.saveCity(city);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 解析和处理服务器返回的县级数据
+	 */
+	public synchronized static boolean handleCountiesResponse(
+			CoolWeatherDB coolWeatherDB, String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCounties = response.split(",");
+			if (allCounties != null && allCounties.length > 0) {
+				for (String p : allCounties) {
+					String[] array = p.split("\\|");
+					County county = new County();
+					county.setCountyCode(array[0]);
+					county.setCountyName(array[1]);
+					county.setCityId(cityId);
+					coolWeatherDB.saveCounty(county);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+}
